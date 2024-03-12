b188ea62f5fb157280d40bccb0a18e45c48cfc5b
Ron111104 <rc071011ggmu@gmail.com>
2024-03-12 16:17:29 +0530

    Project-Par 1 Done Successfully

---
+++ public/js/map.js
@@ -0,0 +1,13 @@
+// mapboxgl.accessToken = mapToken;
+// console.log(mapToken);
+// const map = new mapboxgl.Map({
+//     container: 'map', // container ID
+//     center: coordinates, // starting position [lng, lat]
+//     zoom: 9 ,// starting zoom
+//     // style:"mapbox://styles/mapbox/streets-v12",
+// });
+
+// console.log(coordinates);
+// //const marker= new mapboxgl.Marker()
+//   //  .setLngLat(coordinates)
+//     //.addTo(map);
